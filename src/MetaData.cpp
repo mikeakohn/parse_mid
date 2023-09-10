991d2d9a1e60172255328f6269fa2af9328c76a3
Michael Kohn <mike@mikekohn.net>
2023-09-10 18:51:17 -0500

    Removing some worthless code.

--- src/MetaData.cpp
+++ src/MetaData.cpp
@@ -49,105 +49,11 @@ int MetaData::process(uint8_t *data, int &length)
 
 int MetaData::get_meta_event(uint8_t *data, int &length)
 {
-  length = 0;
-
-  switch (data[0])
-  {
-    case META_SEQUENCE_NUMBER:
-      length = data[1];
-      return 0x00;
-    case META_TEXT_EVENT:
-      return 0x01;
-    case META_COPYRIGHT:
-      return 0x02;
-    case META_TRACK_NAME:
-      return 0x03;
-    case META_INSTRUMENT_NAME:
-      return 0x04;
-    case META_LYRIC_TEXT:
-      return 0x05;
-    case META_MARKER_TEXT:
-      return 0x06;
-    case META_CUE_POINT:
-      return 0x07;
-    case META_CHANNEL_PREFIX:
-      return 0x20;
-    case META_END_OF_TRACK:
-      return 0x2f;
-    case META_TEMPO:
-      return 0x51;
-    case META_SMPTE_OFFSET:
-      return 0x54;
-    case META_TIME_SIGNATURE:
-      return 0x58;
-    case META_KEY_SIGNATURE:
-      return 0x59;
-    case META_SEQUENCER_SPECIFIC:
-      return 0x7f;
-  }
-
   return -1;
 }
 
 int MetaData::dump_meta_event(uint8_t *data)
 {
-  int length;
-  int count;
-  int n;
-
-  switch (data[0])
-  {
-    case META_SEQUENCE_NUMBER:
-      if (data[1] == 0)
-      {
-        printf("MetaEvent sequence_number:\n");
-      }
-        else
-      {
-        printf("MetaEvent sequence_number: %02x %02x\n", data[2], data[3]);
-      }
-
-      break;
-    case META_TEXT_EVENT:
-      printf("MetaEvent text: ");
-
-      length = get_vlength(data + 1, count);
-
-      for (n = 0; n < length; n++)
-      {
-        printf("%c", data[1 + count + n]);
-      }
-
-      printf("\n");
-      break;
-    case META_COPYRIGHT:
-      return 0x02;
-    case META_TRACK_NAME:
-      return 0x03;
-    case META_INSTRUMENT_NAME:
-      return 0x04;
-    case META_LYRIC_TEXT:
-      return 0x05;
-    case META_MARKER_TEXT:
-      return 0x06;
-    case META_CUE_POINT:
-      return 0x07;
-    case META_CHANNEL_PREFIX:
-      return 0x20;
-    case META_END_OF_TRACK:
-      return 0x2f;
-    case META_TEMPO:
-      return 0x51;
-    case META_SMPTE_OFFSET:
-      return 0x54;
-    case META_TIME_SIGNATURE:
-      return 0x58;
-    case META_KEY_SIGNATURE:
-      return 0x59;
-    case META_SEQUENCER_SPECIFIC:
-      return 0x7f;
-  }
-
   return 0;
 }
 
--- src/MetaData.h
+++ src/MetaData.h
@@ -26,25 +26,6 @@ public:
   int dump_meta_event(uint8_t *data);
   void dump();
 
-  enum
-  {
-    META_SEQUENCE_NUMBER    = 0x00,
-    META_TEXT_EVENT         = 0x01,
-    META_COPYRIGHT          = 0x02,
-    META_TRACK_NAME         = 0x03,
-    META_INSTRUMENT_NAME    = 0x04,
-    META_LYRIC_TEXT         = 0x05,
-    META_MARKER_TEXT        = 0x06,
-    META_CUE_POINT          = 0x07,
-    META_CHANNEL_PREFIX     = 0x20,
-    META_END_OF_TRACK       = 0x2f,
-    META_TEMPO              = 0x51,
-    META_SMPTE_OFFSET       = 0x54,
-    META_TIME_SIGNATURE     = 0x58,
-    META_KEY_SIGNATURE      = 0x59,
-    META_SEQUENCER_SPECIFIC = 0x7f,
-  };
-
 private:
   void set_tempo(int value);
 
